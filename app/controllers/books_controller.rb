3d085e4e64fcd16bc480d37a73fe7a922561d3d4
EC2 Default User <ec2-user@ip-172-31-38-29.ap-northeast-1.compute.internal>
2022-07-12 07:55:52 +0000

    showページが上手くいかない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,24 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path(bookid)
+    # book/showページに移動
+    redirect_to book_path(@book)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    # 削除するBookレコードを取得
+    @post_image = Book.find(params[:id])
+    @post_image.destroy
+    # PostImageの一覧ページへのパス
+    redirect_to books_path
   end
 
     # 投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
  root to: "home#top"
  devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :index]
 
  get "/home/about" => "home#about"
